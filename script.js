12b7cae32fcc173ad54ab67d262d4d3ed2ba0c6c
BaturayCaglasin <baturaycaglasin@gmail.com>
2022-04-18 21:22:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//. -> class
+//# -> id
+
+console.log(document.querySelector(".message")); //class'ı message olan element'i seçer. DOM manipulation.
+//console'da:<p class="message"> start guesing...
+console.log(document.querySelector(".between")); //class'ı between olan element'i seçer. DOM manipulation.
+//console'da: <p class="between"> Between 1 and 20...
